666f1241ed3434cac8a4df052282bff811f32121
NaskoVasilev <nasko_vasilev@abv.bg>
2019-07-15 16:17:30 +0300

    AuthService to work with identity more abstract

---
+++ Authentication/BlazorAuthentication.Client/Infrastructure/IAuthService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorAuthentication.Client.Infrastructure
+{
+    public interface IAuthService
+    {
+    }
+}
